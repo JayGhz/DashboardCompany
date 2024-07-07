c93c7c84981d21f79e0674f23d220b4045d03149
JayGhz <jaykansas7@gmail.com>
2024-07-07 10:13:12 -0500

    feat: Add SidebarItem component and update SidebarRoutes
    The code changes introduce a new component called SidebarItem, which is used in the SidebarRoutes component to render sidebar items. The SidebarItem component displays a label, an icon, and a link. The SidebarRoutes component is updated to use the SidebarItem component to render the sidebar items for the GENERAL, TOOLS, and SUPPORT sections.
    
    This commit message follows the convention of starting with a type (feat for feature) and providing a concise summary of the changes made.

--- components/SidebarRoutes/SidebarRoutes.tsx
+++ components/SidebarRoutes/SidebarRoutes.tsx
@@ -1,3 +1,11 @@
+"use client";
+
+import { Separator } from "@/components/ui/separator";
+import { Button } from "@/components/ui/button";
+
+
+import { SidebarItem } from "../SidebarItem";
+
 import { dataSupportSidebar, dataToolsSidebar, SidebarRoutesData } from "./SidebarRoutes.data";
 
 export function SidebarRoutes() {
@@ -5,11 +13,36 @@ export function SidebarRoutes() {
         <div className="flex flex-col justify-between h-full">
             <div>
                 <div className="p-2 md:p-6">
-                    <p>GENERAL</p>
+                    <p className="text-slate-500 mb-2">GENERAL</p>
                     {SidebarRoutesData.map((item) => (
-                        <p key={item.label}>{item.label}</p>
+                        <SidebarItem key={item.label} item={item} />
                     ))}
                 </div>
+                <Separator />
+                <div className="p-2 md:p-6">
+                    <p className="text-slate-500 mb-2">TOOLS</p>
+                    {dataToolsSidebar.map((item) => (
+                        <SidebarItem key={item.label} item={item} />
+                    ))}
+                </div>
+                <Separator />
+                <div className="p-2 md:p-`">
+                    <p className="text-slate-500 mb-2">SUPPORT</p>
+                    {dataSupportSidebar.map((item) => (
+                        <SidebarItem key={item.label} item={item} />
+                    ))}
+                </div>
+            </div>
+            <div>
+                <div className="text-center p-6">
+                    <Button variant="outline" className="w-full">
+                        Upgrade Plan
+                    </Button>
+                </div>
+                <Separator />
+                <footer className="mt-3 p-3 text-center">
+                    2024 © All rights reserved
+                </footer>
             </div>
         </div>
     )
