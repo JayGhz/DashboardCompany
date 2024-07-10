0458f9adaaea0cb64dbd3437adb709da9cd12c49
JayGhz <jaykansas7@gmail.com>
2024-07-10 14:29:51 -0500

    feat: Add TotalSuscribers component and update Home page
    The code changes add a new component called TotalSuscribers, which displays a pie chart of total subscribers across different platforms. The Home page is updated to include the TotalSuscribers component in a new section. This enhancement improves the user experience by providing visual representation of subscriber distribution.

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -2,6 +2,7 @@ import { CardSummary } from "./components/CardSummary";
 import { BookOpenCheck, UserRound, Waypoints } from "lucide-react";
 import { LastCustomers } from "./components/LastCustomers";
 import { SalesDistributor } from "./components/SalesDistributor";
+import { TotalSuscribers } from "./components/TotalSuscribers";
 
 export const dataCardsSummary = [
   {
@@ -46,6 +47,10 @@ export default function Home() {
       <div className="grid grid-cols-1 mt-12 xl:grid-cols-2 md:gap-x-10">
         <LastCustomers />
         <SalesDistributor />
+      </div>
+      <div className="flex-col xl:flex xl:flex-row gap-y-4 md:gap-y-0 mt-12 md:mb-10 justify-center">
+          <TotalSuscribers />
+          <p>List Integrations</p>
       </div>
     </div>
   );
